30f56a1beadc993b0b872a229f1ac8fa6ea3b705
Oppong <josephoppong253@gmail.com>
2024-02-08 02:38:14 +0000

    Home-page updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,29 @@
 import { Button, Container } from "react-bootstrap";
+import "./Home.css";
+import Foot2 from "../../assets/foot2.png";
 
 const Home = () => {
   return (
     <>
-      <Container>
+      <Container className="mt-5">
         <div className="row">
-          <div className="col-md">
-            <span>Hello, new friend!</span>
-            <span>
-              We connect animals and humans - and have done so for many years
-              with passion and dedication. We look forward to introducing you to
-              our daily work and animals!
-            </span>
-            <Button></Button>
-            <Button></Button>
+          <div className="col-md position-relative">
+            <div className="paw">
+              <img src={Foot2} alt="" className="paw1 img-fluid" />
+            </div>
+            <div className="overlay-text">
+              <span>Hello, new friend!</span>
+              <br /> <br />
+              <span>
+                We connect animals and humans - and have done so for many years
+                with passion and dedication. We look forward to introducing you
+                to our daily work and animals!
+              </span>
+              <Button></Button>
+              <Button></Button>
+            </div>
           </div>
-          
+
           <div className="col-md">
             <span>Hello, new friend!</span>
             <span>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import { Button, Navbar, Nav, Container } from 'react-bootstrap';
-import './Navbar.css'
+import { Button, Navbar, Nav, Container } from "react-bootstrap";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 function NavExample() {
   return (
     <>
-      <Navbar className='navbg' bg="light" data-bs-theme="light">
+      <Navbar className="navbg" bg="#" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">MyPet</Navbar.Brand>
+          <Navbar.Brand href="#home" className="logo">MyPet </Navbar.Brand>
+          <FontAwesomeIcon
+            icon={faPaw}
+            color="#e59500"
+            style={{ marginTop: "-20px", marginLeft: "-15px" }}
+          />
           <Nav className="ms-auto">
-            <Nav.Link href="#home">our animals</Nav.Link>
-            <Nav.Link href="#features">blog</Nav.Link>
-            <Nav.Link href="#pricing">contact</Nav.Link>
+            <Nav.Link href="#home" className="link">our animals</Nav.Link>
+            <Nav.Link href="#features" className="link">blog</Nav.Link>
+            <Nav.Link href="#pricing" className="link">contact</Nav.Link>
           </Nav>
-          <Button className='button'>get in touch</Button>
+          <Button className="button">get in touch</Button>
         </Container>
       </Navbar>
     </>
