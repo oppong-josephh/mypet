fe5e3480c4c593e12f1ddb00e38bacbb4d7a7566
Oppong <josephoppong253@gmail.com>
2024-02-10 19:07:28 +0000

    Animals page updated

--- src/components/Animals/Animals.jsx
+++ src/components/Animals/Animals.jsx
@@ -1,34 +1,48 @@
-import { Container } from "react-bootstrap"
-import Dogs from "../../assets/dogs.png"
-import './Animal.css'
-
+import { Container } from "react-bootstrap";
+import Dogs from "../../assets/dogs.png";
+import "./Animals.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 const Animals = () => {
   return (
     <>
-        <Container>
+      <Container>
         <div className="row">
-          <div className="col-md mt-5">
+          <div className="col-md-5 mt-5">
             <div className="a-banner">
               <img className="img-fluid" src={Dogs} alt="" />
             </div>
           </div>
 
-          <div className="col-md mt-5">
+          <div className="col-md-7 mt-5">
             <div className="animals-text">
-              <span>Hello, new friend!</span>
+              <span>Welcome to MyPet</span>
               <br /> <br />
               <span>
-                We connect animals and humans - and have done so for many years
-                with passion and dedication. We look forward to introducing you
-                to our daily work and animals!
+                The reason why animals need our help are very diverse. Our
+                shelter is mainly a refuge for pets in need, such, dogs, cats,
+                small animals, birds and exotics. However, we also provide first
+                aid and care for any wild animals. Many of our charges have a
+                sad history with them and our task is to find a loving home and
+                the right match for them.
+              </span>
+              <br /> <br />
+              <span>
+                On <strong>MyPet</strong>{" "}
+                <FontAwesomeIcon
+                  icon={faPaw}
+                  color="#e59500"
+                  style={{ marginTop: "-20px", marginLeft: "-15px" }}
+                />
+                you have the following benefits:
               </span>
             </div>
           </div>
         </div>
-        </Container>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Animals
+export default Animals;
