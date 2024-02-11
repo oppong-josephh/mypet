ad1a766243e8e476d4c7ca1b4f946e9c9fdb7d7b
Oppong <josephoppong253@gmail.com>
2024-02-11 03:01:45 +0000

    Blog page updated

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
-import { Container, Card, Button } from "react-bootstrap";
+import "./Blog.css";
+import { Container, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaw } from "@fortawesome/free-solid-svg-icons";
 import Cat5 from "../../assets/cat5.png";
@@ -35,43 +36,99 @@ const Blog = () => {
           <div className="col-md-6"></div>
 
           <div className="col-md-4 blog-imgs mt-4">
-            <Card style={{ width: "18rem" }} className="border-0">
-              <Card.Img variant="top" src={Cat5} />
+            <Card
+              style={{ width: "18rem" }}
+              className="border-0 blog-custom-card"
+            >
+              <Card.Img
+                variant="top"
+                src={Cat5}
+                className="blog-custom-card2"
+              />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>6 reasons why your cat licks your hair</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
+                  Did you ever wonder what it means when your cat have multiple
+                  reasons and we...
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <span>
+                  read more &nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon
+                    icon={faPaw}
+                    color="#e59500"
+                    style={{
+                      marginLeft: "-15px",
+                      fontSize: "15px",
+                      marginBottom: "5px",
+                    }}
+                  />
+                </span>
               </Card.Body>
             </Card>
           </div>
 
           <div className="col-md-4 blog-imgs mt-4">
-            <Card style={{ width: "18rem" }} className="border-0">
-              <Card.Img variant="top" src={Dog3} />
+            <Card
+              style={{ width: "18rem" }}
+              className="border-0 blog-custom-card"
+            >
+              <Card.Img
+                variant="top"
+                src={Dog3}
+                className="blog-custom-card2"
+              />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>
+                  What dog bed is the best for my little friend?
+                </Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the cards content.
+                  The choice is enormous. From premium luxury beds to very
+                  simple. What should I look for and how...
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <span>
+                  read more &nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon
+                    icon={faPaw}
+                    color="#e59500"
+                    style={{
+                      marginLeft: "-15px",
+                      fontSize: "15px",
+                      marginBottom: "5px",
+                    }}
+                  />
+                </span>
               </Card.Body>
             </Card>
           </div>
 
           <div className="col-md-4 blog-imgs mt-4">
-            <Card style={{ width: "18rem" }} className="border-0">
-              <Card.Img variant="top" src={Cat4} />
+            <Card
+              style={{ width: "18rem" }}
+              className="border-0 blog-custom-card"
+            >
+              <Card.Img
+                variant="top"
+                src={Cat4}
+                className="blog-custom-card2"
+              />
               <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the cards content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <span>
+                  read more &nbsp;&nbsp;&nbsp;
+                  <FontAwesomeIcon
+                    icon={faPaw}
+                    color="#e59500"
+                    style={{
+                      marginLeft: "-15px",
+                      fontSize: "15px",
+                      marginBottom: "5px",
+                    }}
+                  />
+                </span>
               </Card.Body>
             </Card>
           </div>
